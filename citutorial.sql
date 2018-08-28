a002476ccd8c7224c7da79ecbe86ba2f0c7b8ba1
Andres Vega <aevega1991@gmail.com>
2018-08-27 21:36:52 -0400

    mejorada la base de datos

--- citutorial.sql
+++ citutorial.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Generation Time: Aug 26, 2018 at 02:02 PM
+-- Generation Time: Aug 27, 2018 at 10:35 PM
 -- Server version: 5.7.23-0ubuntu0.16.04.1
 -- PHP Version: 7.0.30-0ubuntu0.16.04.1
 
@@ -21,6 +21,93 @@ SET time_zone = "+00:00";
 --
 CREATE DATABASE IF NOT EXISTS `citutorial` DEFAULT CHARACTER SET latin1 COLLATE latin1_swedish_ci;
 USE `citutorial`;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `cliente`
+--
+
+CREATE TABLE `cliente` (
+  `cli_id` int(11) NOT NULL,
+  `idPersona` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `compania`
+--
+
+CREATE TABLE `compania` (
+  `com_codigo` int(11) NOT NULL,
+  `com_nombre` varchar(45) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `compras`
+--
+
+CREATE TABLE `compras` (
+  `comp_id` int(11) NOT NULL,
+  `comp_proveedor` int(11) NOT NULL,
+  `op_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `inventario`
+--
+
+CREATE TABLE `inventario` (
+  `inv_codigo` int(11) NOT NULL,
+  `inv_item` varchar(45) NOT NULL,
+  `inv_cantidad` int(11) NOT NULL,
+  `inv_operacion_id` int(11) NOT NULL,
+  `inv_tipo_operacion` varchar(10) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `listafactura`
+--
+
+CREATE TABLE `listafactura` (
+  `item_id` int(11) NOT NULL,
+  `param_codigo` int(11) NOT NULL,
+  `item_cantidad` int(11) NOT NULL,
+  `op_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `operacion`
+--
+
+CREATE TABLE `operacion` (
+  `op_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `parametros`
+--
+
+CREATE TABLE `parametros` (
+  `param_codigo` int(11) NOT NULL,
+  `param_nombre` varchar(45) NOT NULL,
+  `param_unidad` varchar(45) NOT NULL,
+  `param_precio_compra` varchar(45) NOT NULL,
+  `param_creado_por` int(11) NOT NULL,
+  `param_fecha_creacion` date NOT NULL,
+  `param_fecha_actualizacion` date NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
 
@@ -43,7 +130,32 @@ CREATE TABLE `persona` (
 --
 
 INSERT INTO `persona` (`idPersona`, `nombre`, `appaterno`, `apmaterno`, `email`, `dni`, `direccion`) VALUES
-(11, 'Andres', 'Vega', 'Vega', 'andres_vega93@hotmail.com', '19422581', 'Urb. La Humbolt');
+(11, 'Andres', 'Vega', 'Vega', 'andres_vega932@hotmail.com', '19422581', 'Urb. La Humbolt'),
+(12, 'test', 'test', 'test', 'test@test.com', '123123', 'asdasdas');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `proveedor`
+--
+
+CREATE TABLE `proveedor` (
+  `prov_id` int(11) NOT NULL,
+  `idPersona` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `sucursal`
+--
+
+CREATE TABLE `sucursal` (
+  `suc_codigo` int(11) NOT NULL,
+  `suc_compania_codigo` int(11) NOT NULL,
+  `suc_nombre` varchar(45) NOT NULL,
+  `suc_direccion` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
 
@@ -66,17 +178,90 @@ CREATE TABLE `usuario` (
 --
 
 INSERT INTO `usuario` (`idUsuario`, `nomUsuario`, `clave`, `idPersona`, `privilegio`, `usr_fec_creacion`, `usr_fec_actualizacion`) VALUES
-(4, 'Andres', '40bd001563085fc35165329ea1ff5c5ecbdbbeef', 11, 'user', '2018-08-18', '2018-08-18');
+(5, 'test', '40bd001563085fc35165329ea1ff5c5ecbdbbeef', 12, 'user', '2018-08-26', '2018-08-26');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `ventas`
+--
+
+CREATE TABLE `ventas` (
+  `vent_codigo` int(11) NOT NULL,
+  `vent_cliente` int(11) NOT NULL,
+  `op_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Indexes for dumped tables
 --
 
 --
+-- Indexes for table `cliente`
+--
+ALTER TABLE `cliente`
+  ADD PRIMARY KEY (`cli_id`),
+  ADD KEY `idPersona` (`idPersona`);
+
+--
+-- Indexes for table `compania`
+--
+ALTER TABLE `compania`
+  ADD PRIMARY KEY (`com_codigo`);
+
+--
+-- Indexes for table `compras`
+--
+ALTER TABLE `compras`
+  ADD PRIMARY KEY (`comp_id`),
+  ADD KEY `op_id` (`op_id`),
+  ADD KEY `compras_ibfk_2` (`comp_proveedor`);
+
+--
+-- Indexes for table `inventario`
+--
+ALTER TABLE `inventario`
+  ADD PRIMARY KEY (`inv_codigo`);
+
+--
+-- Indexes for table `listafactura`
+--
+ALTER TABLE `listafactura`
+  ADD PRIMARY KEY (`item_id`),
+  ADD KEY `listafactura_ibfk_1` (`op_id`),
+  ADD KEY `param_codigo` (`param_codigo`);
+
+--
+-- Indexes for table `operacion`
+--
+ALTER TABLE `operacion`
+  ADD PRIMARY KEY (`op_id`);
+
+--
+-- Indexes for table `parametros`
+--
+ALTER TABLE `parametros`
+  ADD PRIMARY KEY (`param_codigo`);
+
+--
 -- Indexes for table `persona`
 --
 ALTER TABLE `persona`
   ADD PRIMARY KEY (`idPersona`);
+
+--
+-- Indexes for table `proveedor`
+--
+ALTER TABLE `proveedor`
+  ADD PRIMARY KEY (`prov_id`),
+  ADD KEY `idPersona` (`idPersona`);
+
+--
+-- Indexes for table `sucursal`
+--
+ALTER TABLE `sucursal`
+  ADD PRIMARY KEY (`suc_codigo`),
+  ADD KEY `suc_compania_codigo` (`suc_compania_codigo`);
 
 --
 -- Indexes for table `usuario`
@@ -86,28 +271,126 @@ ALTER TABLE `usuario`
   ADD UNIQUE KEY `idPersona` (`idPersona`);
 
 --
+-- Indexes for table `ventas`
+--
+ALTER TABLE `ventas`
+  ADD PRIMARY KEY (`vent_codigo`),
+  ADD KEY `ventas_ibfk_1` (`op_id`),
+  ADD KEY `vent_cliente` (`vent_cliente`);
+
+--
 -- AUTO_INCREMENT for dumped tables
 --
 
 --
+-- AUTO_INCREMENT for table `cliente`
+--
+ALTER TABLE `cliente`
+  MODIFY `cli_id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `compania`
+--
+ALTER TABLE `compania`
+  MODIFY `com_codigo` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `compras`
+--
+ALTER TABLE `compras`
+  MODIFY `comp_id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `inventario`
+--
+ALTER TABLE `inventario`
+  MODIFY `inv_codigo` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `listafactura`
+--
+ALTER TABLE `listafactura`
+  MODIFY `item_id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `operacion`
+--
+ALTER TABLE `operacion`
+  MODIFY `op_id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `parametros`
+--
+ALTER TABLE `parametros`
+  MODIFY `param_codigo` int(11) NOT NULL AUTO_INCREMENT;
+--
 -- AUTO_INCREMENT for table `persona`
 --
 ALTER TABLE `persona`
-  MODIFY `idPersona` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
+  MODIFY `idPersona` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
+--
+-- AUTO_INCREMENT for table `proveedor`
+--
+ALTER TABLE `proveedor`
+  MODIFY `prov_id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `sucursal`
+--
+ALTER TABLE `sucursal`
+  MODIFY `suc_codigo` int(11) NOT NULL AUTO_INCREMENT;
 --
 -- AUTO_INCREMENT for table `usuario`
 --
 ALTER TABLE `usuario`
-  MODIFY `idUsuario` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `idUsuario` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+--
+-- AUTO_INCREMENT for table `ventas`
+--
+ALTER TABLE `ventas`
+  MODIFY `vent_codigo` int(11) NOT NULL AUTO_INCREMENT;
 --
 -- Constraints for dumped tables
 --
 
 --
+-- Constraints for table `cliente`
+--
+ALTER TABLE `cliente`
+  ADD CONSTRAINT `cliente_ibfk_1` FOREIGN KEY (`idPersona`) REFERENCES `persona` (`idPersona`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `compras`
+--
+ALTER TABLE `compras`
+  ADD CONSTRAINT `compras_ibfk_1` FOREIGN KEY (`op_id`) REFERENCES `operacion` (`op_id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `compras_ibfk_2` FOREIGN KEY (`comp_proveedor`) REFERENCES `proveedor` (`prov_id`);
+
+--
+-- Constraints for table `listafactura`
+--
+ALTER TABLE `listafactura`
+  ADD CONSTRAINT `listafactura_ibfk_1` FOREIGN KEY (`op_id`) REFERENCES `operacion` (`op_id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `listafactura_ibfk_2` FOREIGN KEY (`param_codigo`) REFERENCES `parametros` (`param_codigo`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `proveedor`
+--
+ALTER TABLE `proveedor`
+  ADD CONSTRAINT `proveedor_ibfk_1` FOREIGN KEY (`idPersona`) REFERENCES `persona` (`idPersona`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `sucursal`
+--
+ALTER TABLE `sucursal`
+  ADD CONSTRAINT `sucursal_ibfk_1` FOREIGN KEY (`suc_compania_codigo`) REFERENCES `compania` (`com_codigo`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
 -- Constraints for table `usuario`
 --
 ALTER TABLE `usuario`
-  ADD CONSTRAINT `usuario_ibfk_1` FOREIGN KEY (`idPersona`) REFERENCES `persona` (`idPersona`);
+  ADD CONSTRAINT `usuario_ibfk_1` FOREIGN KEY (`idPersona`) REFERENCES `persona` (`idPersona`),
+  ADD CONSTRAINT `usuario_ibfk_2` FOREIGN KEY (`idPersona`) REFERENCES `persona` (`idPersona`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `ventas`
+--
+ALTER TABLE `ventas`
+  ADD CONSTRAINT `ventas_ibfk_1` FOREIGN KEY (`op_id`) REFERENCES `operacion` (`op_id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `ventas_ibfk_2` FOREIGN KEY (`vent_cliente`) REFERENCES `cliente` (`cli_id`);
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
